98b8cf5d06f50833b3520705ced453dfd2d2fe11
T0488018_Eddy <T0488018@my.ntu.ac.uk>
2026-04-18 12:30:36 +0100

    Add import for regex Pattern class

--- FinalSubmission_JourneyManager.java
+++ FinalSubmission_JourneyManager.java
@@ -18,6 +18,7 @@ import java.util.HashMap;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 public class JourneyManager {
 
     private static final String JOURNEYS_FILE = "journeys.csv";
